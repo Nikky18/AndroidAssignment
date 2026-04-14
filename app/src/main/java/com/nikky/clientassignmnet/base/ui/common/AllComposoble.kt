33f9eee28677e007f1c026b2484889c18ab9e93c
Nikky18 <nikkyjain1998@gmail.com>
2026-04-14 16:22:14 +0530

    added loadingView

--- app/src/main/java/com/nikky/clientassignmnet/base/ui/common/AllComposoble.kt
+++ app/src/main/java/com/nikky/clientassignmnet/base/ui/common/AllComposoble.kt
@@ -2,11 +2,13 @@ package com.nikky.clientassignmnet.base.ui.common
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -52,5 +55,19 @@ fun TopBarView(
                 containerColor = MaterialTheme.colorScheme.primaryContainer
             )
         )
+    }
+}
+
+@Composable
+fun LoadingView(
+    isLoading: Boolean? = false,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Box {
+        content()
+        if (isLoading == true) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
     }
 }
